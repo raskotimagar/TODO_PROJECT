a86f30b07bd44e453cb565fd9e2864ce398530a9
Raskoti Magar <raskotimagar8.8.8@gmail.com>
2025-01-23 16:18:44 +0545

    Ready for Render Deployment

--- project_todo/project_todo/settings.py
+++ project_todo/project_todo/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,9 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False  # We are goin for the production so i set i false...
+DEBUG = False # We are goin for the production so i set i false...
 
-ALLOWED_HOSTS = ['project_todo.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['raskoti-todo.onrender.com', 'localhost','127.0.0.1']
 
 
 # Application definition
@@ -80,10 +81,7 @@ WSGI_APPLICATION = 'project_todo.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
@@ -122,10 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://raskoti-todo.onrender.com']
